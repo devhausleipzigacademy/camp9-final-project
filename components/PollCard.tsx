32ae9b5cde7cf9a05b155fbee109e8d5681cb93f
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-06-06 17:34:49 +0200

    fixed display days

--- components/PollCard.tsx
+++ components/PollCard.tsx
@@ -1,53 +1,53 @@
 import { BsArrowUpRight } from 'react-icons/bs';
+
 interface PollCardProps {
   question: string;
   open: boolean;
   voted: boolean;
-  minutes: number;
+  days: number;
   hours: number;
+  minutes: number;
 }
-
 const date = new Date();
-// date in the future
-date.setHours(date.getHours() + 2);
-date.setMinutes(date.getMinutes() + 35);
-
-const countdownHours = date.getHours();
-const countdownMinutes = date.getMinutes();
-
-//
-date.setHours(date.getHours() - 2);
-date.setMinutes(date.getMinutes() - 35);
-
-const countdownHoursPast = date.getHours();
-const countdownMinutesPast = date.getMinutes();
 
 export default function PollCard({
-  question: title,
+  question,
   open = true,
   voted = false,
-  minutes = open ? countdownMinutes : countdownMinutesPast,
-  hours = open ? countdownHours : countdownHoursPast,
+  days = open ? date.getDay() + 1 : date.getDay() - 1,
+  hours = open ? date.getHours() : date.getHours(),
+  minutes = open ? date.getMinutes() : date.getMinutes(),
 }: PollCardProps) {
+  const displayDays = days > 0 ? days : null;
   const displayHours = hours > 0 ? hours : null;
   const displayMinutes = minutes > 0 ? minutes : null;
   const pluralize = (value: number, unit: string) => {
     return value === 1 ? unit : `${unit}s`;
   };
 
+  const closedDate = new Date();
+
+  const closedDateOptions: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
+
   return (
-    <section
-      className={`border-3 border-black rounded w-full h-48 flex flex-col p-4 justify-between bg-yellow shadow-brutalist`}
-    >
-      <div
-        className={`border-3 border-black rounded-md bg-white opacity-70 h-2/3 flex items-center justify-center`}
-      >
-        <h1 className="typography-body">{title}</h1>
+    <section className="border-3 border-black rounded w-full h-48 flex flex-col p-4 justify-between bg-yellow shadow-brutalist PollCard">
+      <div className="border-3 border-black rounded-md bg-white opacity-70 h-2/3 flex items-center justify-center PollCard-content">
+        <h1 className="typography-body">{question}</h1>
       </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between PollCard-footer">
         {open ? (
           <button className="typography-small">
             {`Closes in ${
+              displayDays
+                ? `${displayDays} ${pluralize(displayDays, 'day')}`
+                : ''
+            } ${
               displayHours
                 ? `${displayHours} ${pluralize(displayHours, 'hour')}`
                 : ''
@@ -58,22 +58,16 @@ export default function PollCard({
             }`}
           </button>
         ) : (
-          <button className="typography-small">{`Closed since ${
-            displayHours
-              ? `${displayHours} ${pluralize(displayHours, 'hour')}`
-              : ''
-          } ${
-            displayMinutes
-              ? `${displayMinutes} ${pluralize(displayMinutes, 'minute')}`
-              : ''
-          }`}</button>
+          <button className="typography-small">{`Closed on ${closedDate.toLocaleDateString(
+            undefined,
+            closedDateOptions
+          )}`}</button>
         )}
         {voted ? (
           <button className="typography-body">Voted</button>
         ) : (
           <button className="typography-body flex items-center gap-1">
-            Vote
-            <BsArrowUpRight strokeWidth={'1'} />
+            Vote <BsArrowUpRight strokeWidth={'1'} />
           </button>
         )}
       </div>
