95db119a217fa36f326a13e57788d47b0a8fc9fe
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-06-06 16:43:12 +0200

    created story variants

--- stories/PollCard.stories.tsx
+++ stories/PollCard.stories.tsx
@@ -11,10 +11,26 @@ const meta: Meta<typeof PollCard> = {
 export default meta;
 type Story = StoryObj<typeof PollCard>;
 
-export const Default: Story = {
+export const New: Story = {
   args: {
     question: 'Lorem ipsum dolor sit amet, consectetur?',
     open: true,
     voted: false,
+  },
+};
+
+export const Pending: Story = {
+  args: {
+    question: 'Lorem ipsum dolor sit amet, consectetur?',
+    open: true,
+    voted: true,
+  },
+};
+
+export const Closed: Story = {
+  args: {
+    question: 'Lorem ipsum dolor sit amet, consectetur?',
+    open: false,
+    voted: true,
   },
 };
