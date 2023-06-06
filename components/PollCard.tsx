6009971ea354a04694562a5e320e1015c4996dfb
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-06-06 14:42:09 +0200

    improved styling

--- components/PollCard.tsx
+++ components/PollCard.tsx
@@ -1,5 +1,3 @@
-import { Button } from '../stories/Button';
-
 interface PollCardProps {
   title: string;
 }
@@ -7,19 +5,13 @@ interface PollCardProps {
 export default function PollCard({ title }: PollCardProps) {
   return (
     <section
-      className={`border-4 border-black rounded-md w-80 h-48 flex flex-col p-4 justify-between bg-yellow`}
+      className={`border-3 border-black rounded w-full h-48 flex flex-col p-4 justify-between bg-yellow`}
     >
       <div
-        className={`border-4 border-black rounded-md bg-white opacity-70 h-2/3 flex items-center justify-center`}
+        className={`border-3 border-black rounded-md bg-white opacity-70 h-2/3 flex items-center justify-center`}
       >
         <h1 className="typography-body">{title}</h1>
       </div>
-      <button
-      
-        className={`border-2 border-black rounded-md bg-yellow-100 ml-auto px-2 x`}
-      >
-        ✓
-      </button>
     </section>
   );
 }
