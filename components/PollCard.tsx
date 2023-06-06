aa0649058e23071a92bdad03f66302f98f6d7a55
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-06-06 12:24:07 +0200

    updated styling

--- components/PollCard.tsx
+++ components/PollCard.tsx
@@ -7,15 +7,16 @@ interface PollCardProps {
 export default function PollCard({ title }: PollCardProps) {
   return (
     <section
-      className={`border-2 border-black rounded-md w-80 h-48 flex flex-col p-4 justify-between bg-yellow-500`}
+      className={`border-4 border-black rounded-md w-80 h-48 flex flex-col p-4 justify-between bg-yellow`}
     >
       <div
-        className={`border-2 border-black rounded-md bg-yellow-100 h-2/3 flex items-center justify-center`}
+        className={`border-4 border-black rounded-md bg-white opacity-70 h-2/3 flex items-center justify-center`}
       >
-        <h1 className="font-mono">{title}</h1>
+        <h1 className="typography-body">{title}</h1>
       </div>
       <button
-        className={`border-2 border-black rounded-md bg-yellow-100 ml-auto px-2`}
+      
+        className={`border-2 border-black rounded-md bg-yellow-100 ml-auto px-2 x`}
       >
         ✓
       </button>
