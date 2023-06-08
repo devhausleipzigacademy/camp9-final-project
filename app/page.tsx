cd98ea13c9f01632cda7c1ba06404fbd001a7e79
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-06-08 15:27:24 +0200

    deleted components in page

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,6 @@ export default function Home() {
   return (
     <body className="px-8 flex items-center flex-col ">
       <h1 className="text-3xl text-center text-yellow-900">Consensus App </h1>
-      <PollCard
-        question={'Lorem ipsum dolor sit amet, consectetur?'}
-        voteButton={true}
-        voted={false}
-        dateInput={new Date('2023-06-12T00:00:00')}
-      />
     </body>
   );
 }
