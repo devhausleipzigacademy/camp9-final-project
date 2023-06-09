4ce88ba37c2cea7d5409c1942ad07684f5b63e81
lui <quenaya@web.de>
2023-06-09 14:52:09 +0200

    notready

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,56 +1,55 @@
 'use client';
 
-import { ChangeEvent } from 'react';
-import { useState } from 'react';
 import React from 'react';
 import { clsx } from 'clsx';
+import { forwardRef } from 'react';
 
 type InputFieldProps = {
-  label?: string;
+  label: string;
   placeholder?: string;
-  id: string;
-  max?: number; // Limit the input value to a maximum of characters (max)
+  //max?: number; // Limit the input value to a maximum of characters (max)
   value?: string;
-  type: 'text' | 'number' | 'email' | 'password' | 'search' | 'tel' | 'url';
+  type: 'text' | 'number' | 'email' | 'password' | 'username';
   width: 'full' | 'reduced';
-  register?: any;
-  error: Boolean;
-  errormessage?: string;
-};
-
-export const InputField = ({
-  placeholder,
-  id,
-  label,
-  max,
-}: InputFieldProps) => {
-  const [value, setValue] = useState('');
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.target.value;
-    const truncatedValue = inputValue.slice(0, max);
-    setValue(truncatedValue);
+  error?: {
+    message: string;
   };
-
-  return (
-    <div>
-      <label>
-        {label}
-        <p className="[#FA9A5D]">{errormessage}</p>
-        <input
-          className={clsx(
-            'h-11 border border-black rounded-md placeholder-[#BCBCBC]',
-            width === 'full' ? 'width-[311px]' : 'width-[251px]',
-            error ? 'border-[#FA9A5D]' : 'border-black'
-          )}
-          id={id}
-          value={value}
-          placeholder={placeholder}
-          onChange={handleChange}
-          onFocus={e => (e.target.placeholder = '')}
-        ></input>
-      </label>
-    </div>
-  );
 };
+
+export const InputField = forwardRef(
+  (
+    {
+      //forward ref to pass input to parent (form) and pass back the error to component (inputField)
+      placeholder,
+      label,
+      width,
+      error,
+    }: InputFieldProps,
+    ref
+  ) => {
+    // const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    //   const inputValue = event.target.value;
+    //   const truncatedValue = inputValue.slice(0, max);
+    //   setValue(truncatedValue);
+    // };
+
+    return (
+      <div>
+        <label className="body-semibold">
+          {label}
+          {error && <p className="body-accent">{error.message}</p>}
+          <input
+            className={clsx(
+              ' h-11 border border-black body rounded-md  placeholder-[body-light]',
+              width === 'full' ? 'width-[311px]' : 'width-[251px]',
+              error ? 'border-peach body-accent' : 'border-black body'
+            )}
+            placeholder={placeholder}
+          ></input>
+        </label>
+      </div>
+    );
+  }
+);
 
 export default InputField;
