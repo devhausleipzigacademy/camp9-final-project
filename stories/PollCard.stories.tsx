3b738c6f93045d369a5099cc4734f5569911a706
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-06-12 16:29:41 +0200

    Revert "hided icon"
    This reverts commit 645161c3a062993417f12adc75c537da375f429b.

--- stories/PollCard.stories.tsx
+++ stories/PollCard.stories.tsx
@@ -13,7 +13,7 @@ const meta: Meta<typeof PollCard> = {
     dateInput: {
       control: 'date',
     },
-    icon: { table: { disable: true } },
+    icon: { control: { type: 'exclude' } }, // Exclude the icon control
   },
 };
 
