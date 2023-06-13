2b1c3b99012871f07e8b6eedf24cb9187644b353
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-06-13 12:15:19 +0200

    enforced pages and changed width to percentage

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,23 +1,59 @@
-export default function ProgressBar() {
-  const page = Math.floor(Math.random() * 5) + 1;
+import { cva } from 'class-variance-authority';
 
-  const getWidth = () => {
-    const widths = ['w-1/5', 'w-2/5', 'w-3/5', 'w-4/5', 'w-full'];
-    return widths[page - 1];
-  };
+interface ProgressBarProps {
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  currentPage: number;
+  numberOfPages: number;
+}
 
-  const width = getWidth();
-  const classes = `z-10 border-2 border-red absolute top-0 ${width}`;
+export default function ProgressBar({
+  variant = 'primary',
+  currentPage = 1,
+  numberOfPages = 5,
+}: ProgressBarProps) {
+  // Enforce currentPage to be within the valid range
+  if (currentPage < 1) {
+    currentPage = 1;
+  } else if (currentPage > numberOfPages) {
+    currentPage = numberOfPages;
+  } else if (numberOfPages === 1) {
+    currentPage = 1;
+  }
+
+  // Enforce numberOfPages to be a positive number
+  if (numberOfPages < 1) {
+    numberOfPages = 1;
+  }
+
+  // determine width of progress bar using % values
+  const width =
+    currentPage === numberOfPages
+      ? '100%'
+      : `${(100 / numberOfPages) * currentPage}%`;
+
+  // set up class variance authority
+  const progressBarClasses = cva(['z-10', 'border-2', 'absolute', 'top-0'], {
+    variants: {
+      variant: {
+        primary: ['border-yellow'],
+        secondary: ['border-peach'],
+        tertiary: ['border-green-light'],
+      },
+    },
+  });
+
+  // get classes for progress bar
+  const classes = progressBarClasses({ variant });
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex justify-between items-center">
+    <div className="flex flex-col gap-2 small">
+      <div className="flex justify-between items-center w-full">
         <h3>Progress</h3>
-        <p>{`page ${page} of 5`}</p>
+        <p>{`page ${currentPage} of ${numberOfPages}`}</p>
       </div>
       <div className="relative">
         <hr className="border-2 border-black" />
-        <hr className={classes} />
+        <hr className={classes} style={{ width }} />
       </div>
     </div>
   );
--- stories/Progress.Bar.stories.ts
+++ stories/Progress.Bar.stories.ts
@@ -11,4 +11,37 @@ export default meta;
 
 type Story = StoryObj<typeof ProgressBar>;
 
-export const Default: Story = {};
+export const FirstPage: Story = {
+  args: {
+    currentPage: 1,
+    numberOfPages: 5,
+  },
+};
+
+export const SecondPage: Story = {
+  args: {
+    currentPage: 2,
+    numberOfPages: 5,
+  },
+};
+
+export const ThirdPage: Story = {
+  args: {
+    currentPage: 3,
+    numberOfPages: 5,
+  },
+};
+
+export const FourthPage: Story = {
+  args: {
+    currentPage: 4,
+    numberOfPages: 5,
+  },
+};
+
+export const FifthPage: Story = {
+  args: {
+    currentPage: 5,
+    numberOfPages: 5,
+  },
+};
