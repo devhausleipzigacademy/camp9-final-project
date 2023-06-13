b322f96f5f0801ca7b34fed20ae90f4b314a0c8a
ehidne-mora <mora@slime.garden>
2023-06-13 10:13:34 +0200

    Adds the details folder and page.tsx.

---
+++ app/details/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function PollDetails() {
+  return (
+    <section className="px-8 py-10 bg-yellow h-[667px]">
+      <h1 className="title-black">Poll Details</h1>
+      <div>
+        <h4 className="body-semibold">Poll question</h4>
+        <p className="description">Should we lorem ipsum dolorem sit amet?</p>
+      </div>
+    </section>
+  );
+}
+
+export default PollDetails;
