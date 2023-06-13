764d9887b67d72749342dfd76d0fa9e1fa1e70af
lui <quenaya@web.de>
2023-06-13 11:00:36 +0200

    ready

--- stories/InputField.stories.ts
+++ stories/InputField.stories.ts
@@ -2,16 +2,12 @@ import InputField from '../components/InputField';
 import { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof InputField> = {
-  title: 'InputFieldExample',
+  title: 'components/InputField',
   component: InputField,
   tags: ['autodocs'],
 };
 export default meta;
 type Story = StoryObj<typeof InputField>;
-
-export const Default: Story = {
-  args: { label: 'Custom Label', type: 'text', width: 'full', disabled: false },
-};
 
 export const Placeholder: Story = {
   args: {
@@ -19,44 +15,6 @@ export const Placeholder: Story = {
     placeholder: 'Custom Placeholder',
     type: 'text',
     width: 'full',
-    disabled: false,
-  },
-};
-
-export const disabled: Story = {
-  args: {
-    label: 'Custom Label',
-    type: 'text',
-    width: 'full',
-    disabled: true,
-  },
-};
-
-export const width: Story = {
-  args: {
-    label: 'Custom Label',
-    type: 'text',
-    width: 'reduced',
-    disabled: false,
-  },
-};
-
-export const WithError: Story = {
-  args: {
-    label: 'Custom Label',
-    type: 'password',
-    width: 'full',
-    error: { message: 'Error message' },
-    disabled: false,
-  },
-};
-
-export const WithMax: Story = {
-  args: {
-    label: 'Custom Label',
-    type: 'password',
-    width: 'full',
-    max: 3,
     disabled: false,
   },
 };
