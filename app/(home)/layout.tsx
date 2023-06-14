4d6b958ab0c99c2916c848a7af9ade1351bd110c
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-06-14 12:17:34 +0200

    changed div to main, added previous & next buttons

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -4,8 +4,8 @@ export default function HomeLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="container flex flex-col items-center justify-between min-h-screen bg-peach p-8">
+    <main className="container flex flex-col items-center justify-between min-h-screen bg-peach p-8">
       {children}
-    </div>
+    </main>
   );
 }
--- app/new/page.tsx
+++ app/new/page.tsx
@@ -2,6 +2,32 @@ export default function NewPoll() {
   return (
     <>
       <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
+      <h1 className="title-black">Create New Poll</h1>
     </>
   );
 }
