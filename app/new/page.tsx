a3d803811960ac2b75e8d5a7b4011f6e97b65cf0
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-06-14 12:17:53 +0200

    deleted extra h1s

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -2,32 +2,6 @@ export default function NewPoll() {
   return (
     <>
       <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
-      <h1 className="title-black">Create New Poll</h1>
     </>
   );
 }
