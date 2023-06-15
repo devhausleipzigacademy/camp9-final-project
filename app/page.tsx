661888fb67d96cabcc328277fa2cb4b569a3ee78
mateusmorgado <mateusmorgado@hotmail.com>
2023-06-15 09:52:37 +0200

    adding buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { Questionbox } from 'components/Question';
 import Image from 'next/image';
 import GroupIllustration from '/public/images/Illustr_group_voting.png';
+import Button from 'components/shared/buttons/Button';
+import React from 'react';
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
+
 export default function LandingPage() {
   return (
     <div className="">
@@ -11,6 +15,29 @@ export default function LandingPage() {
         placeholder='blur'
       />
       <div className="text-white text-center text-lg font-medium">Vote secretly, reveal conditionally</div>
+      
+      <Button
+  
+  size="full"
+  variant="secondary"
+>
+  Sign up
+</Button>
+<Button
+
+  size="full"
+  variant="primary"
+>
+  <React.Fragment key=".0">
+    <h3>
+      Login
+    </h3>
+    <HiOutlineArrowNarrowRight
+      size={25}
+      strokeWidth={2}
+    />
+  </React.Fragment>
+</Button>
     </div>
   );
 }
