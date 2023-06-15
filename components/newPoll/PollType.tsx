8d6214f1af5665239ef2fba64d01a299ea3856a5
lui <quenaya@web.de>
2023-06-15 10:28:25 +0200

    n

--- components/newPoll/PollType.tsx
+++ components/newPoll/PollType.tsx
@@ -1,4 +1,6 @@
 'use client';
+type Form = React.FormHTMLAttributes<HTMLFormElement>; //shall it be the same Form/type like in all the cards?
+
 export default function PollType() {
   return <div>PollType</div>;
 }
