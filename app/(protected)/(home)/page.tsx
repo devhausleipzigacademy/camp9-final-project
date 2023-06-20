241731029bbf675b850d3f3b21a73f8ce1bfd442
intertemp <63150529+intertemp@users.noreply.github.com>
2023-06-20 17:35:54 +0200

    reverse merged

--- app/(protected)/(home)/page.tsx
+++ app/(protected)/(home)/page.tsx
@@ -1,12 +1,7 @@
-import ConsensusController from 'components/ConsensusController';
-import PollProgressBar from 'components/PollProgressBar';
-
 export default function Home() {
   return (
     <>
       <h1 className="title-black">Poll Activity</h1>
-      <ConsensusController />
-      <PollProgressBar votes={2} participants={90} />
     </>
   );
 }
