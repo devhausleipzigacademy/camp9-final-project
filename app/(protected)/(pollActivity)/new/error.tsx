978807dc5d2dc3984761a7c463f4594b0ef629c2
intertemp <63150529+intertemp@users.noreply.github.com>
2023-06-20 15:18:42 +0200

    styled the error pages

--- app/(protected)/(pollActivity)/new/error.tsx
+++ app/(protected)/(pollActivity)/new/error.tsx
@@ -3,7 +3,12 @@
 import React from 'react';
 
 function Error() {
-  return <div>Hi from error</div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <img src="/images/flame-479.gif" className="w-[280px]"></img>
+      <h1 className="title-bold text-center">Woops! Something went wrong.</h1>
+    </div>
+  );
 }
 
 export default Error;
