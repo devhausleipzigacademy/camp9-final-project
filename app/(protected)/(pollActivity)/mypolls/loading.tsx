a98ec5f4835896c2db4fd69def477917d88f764e
intertemp <63150529+intertemp@users.noreply.github.com>
2023-06-20 15:10:19 +0200

    added a loading page

--- app/(protected)/(pollActivity)/mypolls/loading.tsx
+++ app/(protected)/(pollActivity)/mypolls/loading.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Loading() {
-  return <div>Hi form Loading</div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <img
+        src="/images/flame-dreaming-of-unicorns.gif"
+        className="w-[280px]"
+      ></img>
+      <h1 className="title-bold">Loading...</h1>
+    </div>
+  );
 }
 
 export default Loading;
