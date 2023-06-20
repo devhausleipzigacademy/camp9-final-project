4dde3fb0a664ac12596895750d7df5c4cc1b7c20
intertemp <63150529+intertemp@users.noreply.github.com>
2023-06-20 09:13:21 +0200

    rocked that consensus bar

--- components/shared/buttons/Controller.tsx
+++ components/shared/buttons/Controller.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+'use client';
+
+import { useEffect, useState } from 'react';
 
 function Controller() {
+  const [value, setValue] = useState(80);
+
+  useEffect(() => {
+    const rangeValue = document.getElementById('rangeValue');
+    if (rangeValue) {
+      rangeValue.style.setProperty('background-size', `${value}%`);
+    }
+  }, [value]);
+
   return (
-    <div>
-      <label></label>
-      <input type="range" min="0" max="100" step="10" list="values"></input>
-      <br />
-      <p>
-        Value: <output id="value"></output>
-      </p>
+    <div className="w-full">
+      <div className="flex flex-row justify-between items-center">
+        <input
+          type="range"
+          min="0"
+          max="100"
+          step="10"
+          list="values"
+          value={value}
+          id="rangeValue"
+          onChange={e => setValue(parseInt(e.target.value))}
+        />
+
+        <p>
+          <output id="outputValue">{value}</output>
+        </p>
+      </div>
       <datalist id="values">
-        <option value="0" label="0">
-          0
-        </option>
-        <option value="50" label="50">
-          50
-        </option>
-        <option value="100" label="100">
-          100
-        </option>
+        <option value="0%" label="0%" />
+        <option value="100%" label="100%" />
       </datalist>
     </div>
   );
