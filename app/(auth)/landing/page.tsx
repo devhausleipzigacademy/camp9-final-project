e1b9b5fe0512089d922e19d364ce11bf88e8bdc4
mateusmorgado <mateusmorgado@hotmail.com>
2023-06-21 12:03:37 +0200

    last edits, putting in the right place

---
+++ app/(auth)/landing/page.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import GroupIllustration from '/public/images/Illustr_group_voting.png';
+import Button from 'components/shared/buttons/Button';
+import React from 'react';
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
+import Link from 'next/link';
+
+export default function LandingPage() {
+  return (
+    <>
+      <h1 className="landing-logo">d'accord</h1>
+      <h2>Landing</h2>
+      <div>
+        <Image
+          src={GroupIllustration}
+          alt="illustration of a group of people making decisions"
+          placeholder='blur'
+        />
+        <div className="text-white text-center text-xl font-medium my-3">Vote secretly, reveal conditionally</div>
+        
+      </div>
+      <div className='w-full'>
+        <Link href="/app/#" >
+          <Button
+          
+            size="full"
+            variant="secondary"
+            className='my-4'
+          >
+            Sign up
+          </Button>
+        </Link>
+        <Link href="/app/login/page">
+          <Button
+          
+            size="full"
+            variant="primary"
+          >
+            <React.Fragment key=".0">
+              <h3>
+          Login
+              </h3>
+              <HiOutlineArrowNarrowRight
+          size={25}
+          strokeWidth={2}
+              />
+            </React.Fragment>
+          </Button>
+        </Link>
+            </div>
+      </>
+  );
+}
--- app/(protected)/landing/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Landing() {
-  return (
-    <>
-      <h1 className="title-black">d'accord</h1>
-    </>
-  );
-}
