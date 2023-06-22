79511dbc56e3e2e1e624d237258fc09703532f02
Doug <dmhwebb@gmail.com>
2023-06-22 11:16:26 +0200

    update for settings data validation/types

--- types/user/AuthSchemata.ts
+++ types/user/AuthSchemata.ts
@@ -1,16 +1,18 @@
 import { z } from 'zod';
 
-export const loginSchema = z.object({
+export const usernameSchema = z.object({
   username: z
     .string()
     .min(3, 'Username must be at least 3 characters long.')
     .max(20, 'Username must be less than 20 characters long.'),
+});
+export type UserType = z.infer<typeof loginSchema>;
+
+export const passwordSchema = z.object({
   password: z.string().min(8, 'Password must be at least 8 characters long.'),
 });
 
-export type LoginSchemaType = z.infer<typeof loginSchema>;
-
-export const signUpSchema = loginSchema
+export const passwordConfirmSchema = passwordSchema
   .extend({
     confirmPassword: z
       .string()
@@ -21,6 +23,12 @@ export const signUpSchema = loginSchema
     path: ['confirmPassword'],
   });
 
+export const loginSchema = usernameSchema.merge(passwordSchema);
+export type LoginSchemaType = z.infer<typeof loginSchema>;
+
+export const signUpSchema = usernameSchema.merge(
+  passwordConfirmSchema.innerType() // <-- innerType fix https://stackoverflow.com/a/74672929
+);
 export type SignUpUser = z.infer<typeof signUpSchema>;
 
 export type SignUpResponse = {
