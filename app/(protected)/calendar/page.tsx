3544469ece5ae0aa2b71b627cb6a768ff4adb0fc
ehidne-mora <mora@slime.garden>
2023-06-23 13:57:14 +0200

    Finishes up time picker.

--- app/(protected)/calendar/page.tsx
+++ app/(protected)/calendar/page.tsx
@@ -1,18 +1,28 @@
 'use client';
-import Button from '@/components/shared/buttons/Button';
-import React, { useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
-import './calendar.css';
 import TimePicker from 'react-time-picker';
 
 function page() {
-  const [date, onChange] = useState(new Date());
-  const [time, onChangeTime] = useState(new Date());
-  const [isCountdown, setIsCountdown] = useState(true);
+  const [dateTime, setDateTime] = useState(new Date());
+  const [showTime, setShowTime] = useState(false);
+  useEffect(() => {
+    setShowTime(true);
+  }, []);
+  function setTime(timeString: string) {
+    const hoursAndMinutes = timeString.split(':');
+    const newDateTime = new Date(dateTime);
+    newDateTime.setHours(
+      parseInt(hoursAndMinutes[0]!),
+      parseInt(hoursAndMinutes[1]!),
+      0
+    );
+    setDateTime(newDateTime);
+  }
   return (
     <main className="px-8 py-10 bg-teal h-[667px]">
       <h1 className="title-black mb-1">Poll Details</h1>
-      <div className="mb-4 flex gap-6">
+      {/* <div className="mb-4 flex gap-6">
         <Button
           variant={isCountdown ? 'primary' : 'quinary'}
           handleClick={() => setIsCountdown(true)}
@@ -25,24 +35,44 @@ function page() {
         >
           Date & Time
         </Button>
-      </div>
-      {isCountdown ? (
-        <></>
-      ) : (
-        <>
-          <Calendar
-            className="shadow-brutal p-4 bg-yellow"
-            prev2Label={null}
-            next2Label={null}
-            minDetail="year"
-            minDate={new Date()}
-            showFixedNumberOfWeeks={true}
-            onChange={() => console.log('New date is: ', date)}
-            value={date}
+      </div> */}
+      <div className="flex flex-col gap-4 items-center">
+        <Calendar
+          className="calendar"
+          prev2Label={null}
+          next2Label={null}
+          minDetail="year"
+          minDate={new Date()}
+          showFixedNumberOfWeeks={true}
+          onChange={() => {}}
+          value={dateTime}
+          navigationAriaLabel="Go up"
+          nextAriaLabel="Next"
+          prevAriaLabel="Previous"
+          aria-label="Calendar"
+        />
+        {showTime && (
+          <TimePicker
+            onContextMenu={e => e.preventDefault()}
+            className="timePicker"
+            amPmAriaLabel="Select AM/PM"
+            clearAriaLabel="Clear value"
+            clockAriaLabel="Toggle clock"
+            hourAriaLabel="Hour"
+            maxDetail="minute"
+            clearIcon={null}
+            minuteAriaLabel="Minute"
+            nativeInputAriaLabel="Time"
+            secondAriaLabel="Second"
+            shouldOpenClock={() => false}
+            value={dateTime}
+            onChange={value => {
+              setTime(value as string);
+            }}
+            onInvalidChange={() => console.log('error!')}
           />
-          <TimePicker onChange={() => {}} value={time} />
-        </>
-      )}
+        )}
+      </div>
     </main>
   );
 }
