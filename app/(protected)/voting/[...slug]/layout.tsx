ca534e94d36efca1a1b6fb127933f1330f50ce89
intertemp <63150529+intertemp@users.noreply.github.com>
2023-06-26 16:37:24 +0200

    fixed last colors

--- app/(protected)/voting/[...slug]/layout.tsx
+++ app/(protected)/voting/[...slug]/layout.tsx
@@ -10,7 +10,7 @@ export default function VotingLayout({
   return (
     <>
       <div className="h-screen w-screen">
-        <main className="page flex flex-col justify-between gap-5 h-full bg-green container ">
+        <main className="page flex flex-col justify-between gap-5 h-full bg-[#9FCC95]">
           {children}
         </main>
         <footer className="fixed bottom-6 container px-8">
