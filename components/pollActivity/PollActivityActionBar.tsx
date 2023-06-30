15e0163af6d70984dee56fbc8305c5503c875c2b
ehidne-mora <mora@slime.garden>
2023-06-30 14:31:57 +0200

    Moves page.tsx to Poll Activity root from /new.

--- components/pollActivity/PollActivityActionBar.tsx
+++ components/pollActivity/PollActivityActionBar.tsx
@@ -10,7 +10,7 @@ function PollActivityActionBar({ className }: PollActivityActionBarProps) {
     <>
       <h1 className="title-black mb-10 px-8">Poll Activity</h1>
       <nav className="flex gap-2 flex-row justify-center mb-5">
-        <TabButton href="/new">new</TabButton>
+        <TabButton href="/">new</TabButton>
         <TabButton href="/pending">pending</TabButton>
         <TabButton href="/closed">closed</TabButton>
         <TabButton href="/mypolls">my polls</TabButton>
