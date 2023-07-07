34803996c7ef5ba067efcca7a43e8eb6eb73b7e4
intertemp <63150529+intertemp@users.noreply.github.com>
2023-07-07 13:35:16 +0200

    fixed landing page of voting to adjust to dofferent question lenghts

--- components/voting/QuestionVote.tsx
+++ components/voting/QuestionVote.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 
 interface QuestionVoteProps {
@@ -8,8 +9,19 @@ interface QuestionVoteProps {
 function QuestionVote(props: QuestionVoteProps) {
   return (
     <div>
-      <div className="questionVote w-full h-auto p-2 border-3 border-solid border-black bg-peach rounded-md">
-        <h3>{props.question}</h3>
+      <div
+        className={clsx(
+          props.description && props.question?.length! >= 50
+            ? 'max-h-[150px] w-[300px] overflow-y-auto scrollbarteal'
+            : 'w-full',
+          !props.description && props.question?.length! >= 50
+            ? 'max-h-[380px] w-[300px] overflow-y-auto scrollbarteal'
+            : 'w-full'
+        )}
+      >
+        <div className="questionVote p-2 border-3 border-solid border-black bg-peach rounded-md mr-2">
+          <h3>{props.question}</h3>
+        </div>
       </div>
       {props.description && (
         <div className="h-[278px] pt-4">
