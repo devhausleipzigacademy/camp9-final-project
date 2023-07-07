47a35bc763a5e4ef4f19903e54a248e68c2916a9
Nnek Nnek <hillmar@posteo.de>
2023-07-07 15:00:33 +0200

    changed navabr colours

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,28 +1,29 @@
+'use client';
+
 import React from 'react';
 import clsx from 'clsx';
 import { IoHomeSharp } from 'react-icons/io5';
 import { IoIosAdd } from 'react-icons/io';
 import { HiUser } from 'react-icons/hi2';
 import Navbaritem from './NavbarItem';
+import { usePathname } from 'next/navigation';
 
 interface NavbarProps {
-  variant: 'primary' | 'secondary';
-  // pri: bg-green-light, sec: bg-teal
   active?: boolean;
   className?: string;
 }
-export const Navbar = ({
-  variant = 'primary',
-  active,
-  className,
-}: NavbarProps) => {
+export const Navbar = ({ active, className }: NavbarProps) => {
+  const path = usePathname();
+  console.log(path);
   return (
     <>
       <nav className="flex justify-center container">
         <div
           className={clsx(
             'flex justify-around items-center w-full h-[60px] rounded-round border-2 border-black border-solid shadow-shadow',
-            variant === 'primary' ? 'bg-green-sage' : 'bg-teal'
+            path.includes('/vote') || path.includes('/details')
+              ? 'bg-teal'
+              : 'bg-green-sage'
           )}
         >
           <Navbaritem hrefString="/">
